43d13cd3f3bc7c8c25d66056f396137dcd269a99
Dan Herbert <no-fprint-dan@hrbrt.io>
2023-09-03 14:41:26 -0700

    Add time zone data back into dev branch
    Was dropped in merge. Re-adding it to keep this feature for the dev
    branch.
    
    This reverts commit 12cce591d461a3012fa31544440f20f1cfa2194f.

--- abf.js
+++ abf.js
@@ -16,6 +16,8 @@
 			(Math.floor(Math.random() * ((max / 2) - min + 1)) + min) * 2
 		const rand = (min, max) =>
 			(Math.floor(Math.random() * (max - min + 1)) + min)
+		// Includes DST zones in the list of values, but does not attempt to figure out if the current date should use DST.
+		const timezoneOffsetComputed = listRand([360, 300, 240, -60, -120, -180, -480])
 		// Device GPU
 		// https://www.primegrid.com/gpu_list.php
 		const webglRenderer = () => {
@@ -118,6 +120,7 @@
 		return {
 			timestamp,
 			hash,
+			timezoneOffsetComputed,
 			webglExtensionComputed,
 			canvasContextComputed,
 			clientRectsOffsetComputed,
@@ -132,6 +135,7 @@
 	const {
 		timestamp,
 		hash,
+		timezoneOffsetComputed,
 		webglExtensionComputed,
 		canvasContextComputed,
 		clientRectsOffsetComputed,
@@ -142,6 +146,12 @@
 	// window
 	const originalDevicePixelRatio = window.devicePixelRatio
 	const fakePixelRatio = Math.round(originalDevicePixelRatio)
+	// Date
+	const nativeGetTimezoneOffset = Date.prototype.getTimezoneOffset
+	function getTimezoneOffset() {
+		nativeGetTimezoneOffset.apply(this)
+		return timezoneOffsetComputed
+	}
 	// Navigator
 	const nativeGetBattery = navigator.getBattery
 	function getBattery() {
@@ -642,7 +652,7 @@
 		name: 'Date',
 		proto: true,
 		struct: {
-			getTimezoneOffset: Date.prototype.getTimezoneOffset
+			getTimezoneOffset: getTimezoneOffset
 		}
 	},
 	{
